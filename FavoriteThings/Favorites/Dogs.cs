d52a1a77069309b53d39fac36a392d468de6bbdf
Caitlin Murnick <caitlinmurnick@gmail.com>
2018-01-12 11:53:40 -0600

    setting up

--- FavoriteThings/Favorites/Dogs.cs
+++ FavoriteThings/Favorites/Dogs.cs
@@ -8,5 +8,37 @@ namespace FavoriteThings.Favorites
 {
     class Dogs
     {
+        protected string _favoriteKindOfDog;
+
+        public string FavoriteKindOfDog
+        {
+            get
+            {
+                return _favoriteKindOfDog;
+            }
+
+            set
+            {
+                _favoriteKindOfDog = value;
+            }
+        }
+    }
+
+    class DogName : Dogs
+    {
+        protected string _dogsName;
+
+        public string FavoriteDogsName
+        {
+            get
+            {
+                return $"My favorite dog's name is {_dogsName}";
+            }
+            set
+            {
+                _dogsName = value;
+            }
+        }
+
     }
 }
--- FavoriteThings/Program.cs
+++ FavoriteThings/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using FavoriteThings.Favorites;
 
 namespace FavoriteThings
 {
@@ -13,7 +10,7 @@ namespace FavoriteThings
             var dogs = new Dogs();
 
             Console.WriteLine("I love dogs!");
-            string inputString = dogs.DogType();
+            string inputString = Dogs();
         }
     }
 }
